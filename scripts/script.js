27a01a70305e85fafc39ea8c17ca9a09aed7c058
Lisa Oude Elferink <57795294+lisaoude@users.noreply.github.com>
2020-09-08 13:06:39 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
 // JavaScript Document
+const hamburger = document.querySelector('.hamburger');
+const line = document.querySelector('.line');
+const navigation = document.querySelector('.navigation');
+
+
+hamburger.addEventListener('click', animateHamburger);
+
+function animateHamburger() {
+    hamburger.classList.toggle('active');
+    navigation.classList.toggle('active');
+};
+
+
+console.log('dit werkt');
